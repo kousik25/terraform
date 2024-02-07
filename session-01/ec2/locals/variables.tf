f7e2eb02a95126a1efe90fde2f343c4a0dc8359d
kousik25 <kousikdittakavi1@gmail.com>
2024-02-07 12:59:12 +0530

    terraform

---
+++ session-01/ec2/locals/variables.tf
@@ -0,0 +1,16 @@
+variable "isProd" {
+  type = bool
+  default = true
+}
+
+variable "ami_id" {
+  default = "ami-0f3c7d07486cad139"
+}
+
+variable "zone_id" {
+  default = "Z04130393O4140Y196OJD"
+}
+
+variable "domain_name" {
+  default = "devopsjoin.website"
+}
